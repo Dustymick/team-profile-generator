cf91d075cb786944ce921d23f3561e92973dd877
DustyMick <damicklo5@gmail.com>
2021-02-17 13:31:56 -0600

    Employee parent code added

---
+++ Index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer")
+const Engineer = require("./lib/Engineer.js")
+const Intern = require("./lib/Intern.js")
+const Manager = require("./lib/Manager.js")
+
+//const dustin = new Employee("Dustin", 1234, "gmail.com")
+//console.log(dustin.getName)
